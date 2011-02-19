f80944a50d7815021d7a1900492d2cb13f2f4774
Jeremy C. Andrus <jandrus@nvidia.com>
2011-02-19 13:25:42 -0500

    fixed fif utility: I wasn't shifting, and find -E doesn't work in Linux

--- bash.d/find_utils.sh
+++ bash.d/find_utils.sh
@@ -7,7 +7,7 @@ __fif() {
 	local DIR=${1:-.}
 	shift
 	until [ -z "$DIR" ]; do
-		find -E "${DIR}" -type f -print0 | xargs -0 grep --color -n "$PAT"
+		find "${DIR}" -type f -print0 | xargs -0 grep --color -n "$PAT"
 		DIR=$1
 		shift
 	done
@@ -15,7 +15,9 @@ __fif() {
 
 # find-in-files: ignore warnings/errors
 fif() {
-	__fif "${1/ /\\ }" $@ 2>/dev/null
+	local PAT=${1/ /\\ }
+	shift
+	__fif "$PAT" $@ 2>/dev/null
 }
 
 # Find a file in the current, or specified directory
